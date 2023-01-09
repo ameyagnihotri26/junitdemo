9b235304a5b15d3d08dfc4f94bab032037ffb563
ameyagnihotri26 <Amey.Agnihotri@hansencx.com>
2023-01-09 16:16:21 +0530

    junit commited

---
+++ src/test/java/com/junitdemo/test/MyTestCase.java
@@ -0,0 +1,10 @@
+package com.junitdemo.test;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTestCase {
+@Test
+    public void sayHello() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/junitdemo/test/MyTestCaseTest.java
@@ -0,0 +1,12 @@
+package com.junitdemo.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTestCaseTest {
+
+    @org.junit.jupiter.api.Test
+    void sayHello() {
+    }
+}
